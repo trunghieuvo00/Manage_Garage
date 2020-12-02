7c58ac660abd8de51adc3c931f69bd9b3fbdc55d
Hieu <18520758@ms.uit.edu.vn>
2020-12-03 04:23:08 +0700

    Thay doi quy dinh (DONE)

--- BrotherGara/Controllers/HIEUXEsController.cs
+++ BrotherGara/Controllers/HIEUXEsController.cs
@@ -38,6 +38,12 @@ namespace BrotherGara.Controllers
         // GET: HIEUXEs/Create
         public ActionResult Create()
         {
+            ViewBag.loi = false;
+            int countHIEUXEs = db.HIEUXEs.ToList().Count();
+            int soXeToiDa = db.THAMSOes.ToList().ElementAt(0).GiaTri;
+            if (countHIEUXEs >= soXeToiDa)
+                ViewBag.loi = true;
+
             return View();
         }
 
@@ -48,7 +54,14 @@ namespace BrotherGara.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "MaHieuXe,TenHieuXe")] HIEUXE hIEUXE)
         {
-            if (ModelState.IsValid)
+            int countHIEUXEs = db.HIEUXEs.ToList().Count();
+            int soXeToiDa = db.THAMSOes.ToList().ElementAt(0).GiaTri;
+            if (countHIEUXEs == soXeToiDa)
+            {
+                return RedirectToAction("Index");
+            }
+
+            else if (ModelState.IsValid && countHIEUXEs < soXeToiDa)
             {
                 db.HIEUXEs.Add(hIEUXE);
                 db.SaveChanges();
@@ -125,3 +138,132 @@ namespace BrotherGara.Controllers
         }
     }
 }
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Net;
+//using System.Web;
+//using System.Web.Mvc;
+//using BrotherGara.Models;
+
+//namespace BrotherGara.Controllers
+//{
+//    public class HIEUXEsController : Controller
+//    {
+//        private BrothersGarageEntities db = new BrothersGarageEntities();
+
+//        // GET: HIEUXEs
+//        public ActionResult Index()
+//        {
+//            return View(db.HIEUXEs.ToList());
+//        }
+
+//        // GET: HIEUXEs/Details/5
+//        public ActionResult Details(string id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            HIEUXE hIEUXE = db.HIEUXEs.Find(id);
+//            if (hIEUXE == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(hIEUXE);
+//        }
+
+//        // GET: HIEUXEs/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
+
+//        // POST: HIEUXEs/Create
+//        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+//        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create([Bind(Include = "MaHieuXe,TenHieuXe")] HIEUXE hIEUXE)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.HIEUXEs.Add(hIEUXE);
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+
+//            return View(hIEUXE);
+//        }
+
+//        // GET: HIEUXEs/Edit/5
+//        public ActionResult Edit(string id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            HIEUXE hIEUXE = db.HIEUXEs.Find(id);
+//            if (hIEUXE == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(hIEUXE);
+//        }
+
+//        // POST: HIEUXEs/Edit/5
+//        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+//        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit([Bind(Include = "MaHieuXe,TenHieuXe")] HIEUXE hIEUXE)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.Entry(hIEUXE).State = EntityState.Modified;
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            return View(hIEUXE);
+//        }
+
+//        // GET: HIEUXEs/Delete/5
+//        public ActionResult Delete(string id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            HIEUXE hIEUXE = db.HIEUXEs.Find(id);
+//            if (hIEUXE == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(hIEUXE);
+//        }
+
+//        // POST: HIEUXEs/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteConfirmed(string id)
+//        {
+//            HIEUXE hIEUXE = db.HIEUXEs.Find(id);
+//            db.HIEUXEs.Remove(hIEUXE);
+//            db.SaveChanges();
+//            return RedirectToAction("Index");
+//        }
+
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                db.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+//    }
+//}
